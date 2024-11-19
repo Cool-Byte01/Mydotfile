7649e0ca7f0abbf659b1e7b125b8583e078d2b1e
Cool-Byte01 <187045662+Cool-Byte01@users.noreply.github.com>
2024-11-19 12:18:33 +0700

    Config NcChad

---
+++ nvim/lua/plugins/init.lua
@@ -0,0 +1,51 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
+
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        -- "lua-language-server",
+        -- "stylua",
+        -- "html-lsp",
+        -- "css-lsp",
+        -- "prettier",
+      },
+    },
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+        "python",
+      },
+    },
+  },
+
+  {
+    "rachartier/tiny-inline-diagnostic.nvim",
+    event = "VeryLazy",
+    config = function()
+      require "configs.inline-diagnostics"
+    end,
+  },
+}
